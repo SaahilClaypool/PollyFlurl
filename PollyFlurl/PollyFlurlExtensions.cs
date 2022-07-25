538ca73b96be4a3e66d6f4a0e6c5102c69d423c9
Saahil Claypool <saahil@claypools.org>
2022-07-24 20:16:59 -0400

    Remove dead code

--- PollyFlurl/PollyFlurlExtensions.cs
+++ PollyFlurl/PollyFlurlExtensions.cs
@@ -5,23 +5,6 @@ using IFlurlHttpClientFactory = Flurl.Http.Configuration.IHttpClientFactory;
 namespace Polly.Flurl;
 public static class PollyFlurlExtensions
 {
-    static readonly HashSet<int> HTTPTransientErrorStatusCodes = new()
-    {
-        (int)HttpStatusCode.InternalServerError,
-        (int)HttpStatusCode.RequestTimeout,
-        (int)HttpStatusCode.BadGateway,
-        (int)HttpStatusCode.GatewayTimeout,
-        (int)HttpStatusCode.ServiceUnavailable,
-    };
-    static readonly HttpStatusCode[] httpStatusCodesWorthRetrying = {
-        HttpStatusCode.RequestTimeout, // 408
-        HttpStatusCode.InternalServerError, // 500
-        HttpStatusCode.BadGateway, // 502
-        HttpStatusCode.ServiceUnavailable, // 503
-        HttpStatusCode.GatewayTimeout // 504
-    };
-
-
     public static IFlurlRequest WithPolicy(this string request, IAsyncPolicy<IFlurlResponse> policy) => WithPolicy(new Url(request), policy);
     public static IFlurlRequest WithPolicy(this Url request, IAsyncPolicy<IFlurlResponse> policy) => WithPolicy(new FlurlRequest(request), policy);
     public static IFlurlRequest WithPolicy(this IFlurlRequest request, IAsyncPolicy<IFlurlResponse> policy)
@@ -42,6 +25,14 @@ public static class PollyFlurlExtensions
     {
         return new PollyHttpResponseRequest(request, policy);
     }
+
+    static readonly HttpStatusCode[] httpStatusCodesWorthRetrying = {
+        HttpStatusCode.RequestTimeout, // 408
+        HttpStatusCode.InternalServerError, // 500
+        HttpStatusCode.BadGateway, // 502
+        HttpStatusCode.ServiceUnavailable, // 503
+        HttpStatusCode.GatewayTimeout // 504
+    };
 
     public static IFlurlRequest WithRetry(this string request) => WithRetry(new Url(request));
     public static IFlurlRequest WithRetry(this Url request) => WithRetry(new FlurlRequest(request));
